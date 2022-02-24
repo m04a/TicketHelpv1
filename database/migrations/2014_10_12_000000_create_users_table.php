a225d0fa364c9f2b92a75ba6f850589adf4a7c75
Àlex Aguilera Ullés <aaguilera@iconsl.com>
2022-02-24 19:44:19 +0100

    start migration user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id()->primaryKey();
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->string('role_id');
+            
+
             $table->rememberToken();
             $table->timestamps();
         });
