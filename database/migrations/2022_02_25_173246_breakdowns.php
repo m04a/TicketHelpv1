bbd9dec23240351c863d97e3981da761b8eccfce
nemesxv <lupenkov14@gmail.com>
2022-02-24 16:37:10 +0100

    creada migracion types, cambiada la fecha de breakdowns

---
+++ database/migrations/2022_02_25_173246_breakdowns.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('breakdowns', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->boolean('status');
+            $table->foreignId('departament_id')
+            ->constrained('departaments');
+            $table->foreignId('user_id')
+            ->constrained('users');
+            $table->foreignId('manager_id')
+            ->constrained('managers')
+            ->nullable();
+            $table->foreignId('device_id')
+            ->constrained('devices')
+            ->nullable();
+            $table->foreignId('zone_id')
+            ->constrained('zones')
+            ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+        public function down()
+    {
+        Schema::dropIfExists('breakdowns');
+    }
+};
