401ca97d7fee38ff384645029119abd77a073fa6
nemesxv <lupenkov14@gmail.com>
2022-02-24 16:43:37 +0100

    migration de mensajes hecha

---
+++ database/migrations/2022_02_24_154010_messages.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->foreignId('user_id')
+            ->constrained('users');
+            $table->foreignId('question_id')
+            ->constrained('questions')
+            ->nullable();
+            $table->foreignId('breakdown_id')
+            ->constrained('breakdowns');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};
