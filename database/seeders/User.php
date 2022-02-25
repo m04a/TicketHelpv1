596fe43af31b4147548cf69651d8a899fb7b146b
Àlex Aguilera Ullés <aaguilera@iconsl.com>
2022-02-25 16:32:26 +0100

    Create seeder of User and implement some users for testing

--- database/seeders/User.php
+++ database/seeders/User.php
@@ -22,53 +22,62 @@ class User extends Seeder
             'role_id' => 3,
             'password' => '1234567890',
         ]);
+
         DB::table('users')->insert([
             'username' => 'Alex',
             'email' => 'Alex@Alex',
             'role_id' => 3,
             'password' => '1234567890',
         ]);
+
         DB::table('users')->insert([
             'username' => 'Moha',
             'email' => 'Moha@Moha',
             'role_id' => 3,
             'password' => '1234567890',
         ]);
+
         DB::table('users')->insert([
             'username' => 'Juanka',
             'email' => 'Juanka@Juanka',
             'role_id' => 3,
             'password' => '1234567890',
         ]);
+
         DB::table('users')->insert([
             'username' => 'Kirill',
             'email' => 'Kirill@Kirill',
             'role_id' => 3,
             'password' => '1234567890',
         ]);
+
         DB::table('users')->insert([
             'username' => 'Jorge',
             'email' => 'Jorge@Jorge',
             'role_id' => 3,
             'password' => '1234567890',
         ]);
+
         DB::table('users')->insert([
             'username' => 'Iker',
             'email' => 'Iker@Iker',
             'role_id' => 3,
             'password' => '1234567890',
         ]);
+        
         DB::table('users')->insert([
             'username' => 'User',
             'email' => 'User@User',
             'role_id' => 1,
             'password' => '1234567890',
         ]);
+
         DB::table('users')->insert([
             'username' => 'Editor',
             'email' => 'Editor@Editor',
             'role_id' => 2,
             'password' => '1234567890',
         ]);
+        
     }
 }
