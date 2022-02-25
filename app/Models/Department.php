f56ad7c5dae53ff1c8a6bfc2e49d09684c9d0223
George11849 <george11849@gmail.com>
2022-02-25 19:12:52 +0100

    #29 department model

---
+++ app/Models/Department.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    use HasFactory;
+
+
+    public function Question()
+    {
+        return $this->hasMany(Question::Class);
+    }
+
+    public function Suggestion()
+    {
+        return $this->hasMany(Suggestion::Class);
+    }
+
+    public function Breakdown()
+    {
+        return $this->hasMany(Breakdown::Class);
+    }
+
+    protected $fillable = [
+        'id',
+        'name',
+    ];
+}
