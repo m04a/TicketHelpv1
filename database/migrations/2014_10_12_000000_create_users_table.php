2c141505dface8039c276be93433501f39c3003b
Àlex Aguilera Ullés <aaguilera@iconsl.com>
2022-02-25 15:04:30 +0100

    Modify migration of Users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id()->primaryKey();
+            $table->id();
             $table->string('username');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
 
-            $table->string('role_id');
-            
+            $table->unsignedBigInteger('role_id')->nullable();
+            $table->index('role_id');
+            $table->foreign('role_id')->references('id')->on('role');
 
             $table->rememberToken();
             $table->timestamps();
@@ -32,8 +34,12 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(Blueprint $table)
     {
         Schema::dropIfExists('users');
+
+        $table->dropForeign('lists_role_id_foreign');
+        $table->dropIndex('lists_role_index');
+        $table->dropColumn('role_id');
     }
 };
