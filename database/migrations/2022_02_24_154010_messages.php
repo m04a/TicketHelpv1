92f210b5d132dc5bf2c11f3df3261b275641a71f
Oriol Bech <52133567+OriolBech@users.noreply.github.com>
2022-02-25 16:20:50 +0100

    Update 2022_02_24_154010_messages.php

--- database/migrations/2022_02_24_154010_messages.php
+++ database/migrations/2022_02_24_154010_messages.php
@@ -16,13 +16,16 @@ return new class extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->text('content');
-            $table->foreignId('user_id')
-            ->constrained('users');
-            $table->foreignId('question_id')
-            ->constrained('questions')
-            ->nullable();
-            $table->foreignId('breakdown_id')
-            ->constrained('breakdowns');
+            
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            
+            $table->unsignedBigInteger('question_id');
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade')->nullable();
+            
+            $table->unsignedBigInteger('breakdown_id');
+            $table->foreign('breakdown_id')->references('id')->on('breakdowns')->onDelete('cascade')->nullable();
+            
             $table->timestamps();
         });
     }
