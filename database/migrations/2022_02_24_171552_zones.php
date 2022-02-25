602f9489431ae28f1a350fac5dc9a7cb691ebd09
Oriol Bech <52133567+OriolBech@users.noreply.github.com>
2022-02-25 16:34:25 +0100

    Update 2022_02_24_171552_zones.php

--- database/migrations/2022_02_24_171552_zones.php
+++ database/migrations/2022_02_24_171552_zones.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('zones', function (Blueprint $table) {
             $table->id();
             $table->string('label');
             $table->string('description');
