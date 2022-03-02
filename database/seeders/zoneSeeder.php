1e43dacef171f8c69453f5491db3001fde109d93
Juanka007 <juancarlos_bvjcbv@hotmail.com>
2022-03-02 19:24:03 +0100

    feature_ZoneSeeder #41

--- database/seeders/zoneSeeder.php
+++ database/seeders/zoneSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\models\Zone;
 
 class ZoneSeeder extends Seeder
 {
@@ -16,19 +17,19 @@ class ZoneSeeder extends Seeder
     public function run()
     {
 
-        DB::table('zones')->insert([
+        Zone::insert([
             'label' => 'Aula1',
             'description' => "aula de 1er d'Eso"]);
 
-        DB::table('zones')->insert([
+        Zone::insert([
             'label' => 'Aula2',
             'description' => "aula de 2on d'Eso"]);
 
-        DB::table('zones')->insert([
+        Zone::insert([
             'label' => 'Aula3',
             'description' => "aula de 3er d'Eso"]);
 
-        DB::table('zones')->insert([
+        Zone::insert([
             'label' => 'Aula4',
             'description' => "aula de 4rt d'Eso"]);
 
