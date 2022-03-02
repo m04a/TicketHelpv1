4f17fa2037b6286e61d4262f8e68d724ee55d371
George11849 <george11849@gmail.com>
2022-03-02 12:17:40 +0100

    #25 message model

---
+++ app/Models/Message.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    public function User()
+    {
+        return $this->belongsTo(User::Class);
+    }
+
+    public function Question()
+    {
+        return $this->belongsTo(Question::Class);
+    }
+
+    public function Breakdown()
+    {
+        return $this->belongsTo(Question::Class);
+    }
+}
