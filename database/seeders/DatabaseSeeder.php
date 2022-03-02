83c4156684f3564bab4740a094a72cb2c43a296e
OriolBech <you@example.com>
2022-03-02 19:22:40 +0100

    Fixed some seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             UserSeeder::class,
             DeviceSeeder::class,
-            DepartmentSeeder::class
+            DepartmentSeeder::class,
+            TypeSeeder::class
         ]);
     }
 }
