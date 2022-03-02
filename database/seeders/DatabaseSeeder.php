2cd2353cba45b3167c6f7e2d63c15818582414bf
George11849 <george11849@gmail.com>
2022-03-02 17:31:49 +0100

    seederDepartment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             RoleSeeder::class,
             DeviceSeeder::class,
+            DepartmentSeeder::class
         ]);
     }
 }
---
+++ database/seeders/DepartmentSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DepartmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('departments')->insert([
+            'name' => 'Departament Informàtica'
+        ]);
+
+        DB::table('departments')->insert([
+            'name' => 'Consergeria'
+        ]);
+
+        DB::table('departments')->insert([
+            'name' => 'Secretaria'
+        ]);
+
+        DB::table('departments')->insert([
+            'name' => 'Direcció'
+        ]);
+
+        DB::table('departments')->insert([
+            'name' => 'Altres'
+        ]);
+    }
+    
+}
