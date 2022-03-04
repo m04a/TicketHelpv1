6b2f52a85956b2191a30292c738eb1e36840ed46
Àlex Aguilera Ullés <aaguilera@iconsl.com>
2022-03-04 16:34:29 +0100

    change structure of views

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h1 class="title">
+            Dashboard
+        </h1>
+    </x-slot>
+</x-app-layout>
+<x-slot name="slot">
+    <p>Està es una prova</p>
+</x-slot>
