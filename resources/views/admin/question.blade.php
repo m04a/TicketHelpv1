8772c521afc411698cf4905c8cc55e92537802d2
Juanka007 <juancarlos_bvjcbv@hotmail.com>
2022-03-07 15:53:58 +0100

    Question Admin Views #77

--- resources/views/admin/question.blade.php
+++ resources/views/admin/question.blade.php
@@ -5,18 +5,32 @@
         </h1>
     </x-slot>
     <x-slot name="slot">
-        <div class="questionTable">
         <section class="section main-section">
-        <div class="card has-table">
-            <header class="card-header">
-                <p class="card-header-title">
-                    <span class="icon"><i class="mdi mdi-account-multiple"></i></span>
-            <div class="myQuestions">
-                <h1>Les meves preguntes</h1>
+            <div class="card has-table">
+                <header class="card-header card-header-question">
+                    <span class="icon">
+                        <i class="mdi mdi-account-multiple"></i>
+                    </span>
+                    <h1 class="card-header-title card-header-title-question">Preguntes a respondre</h1>
+                </header>
+                <div>
+                    HHHH
+                </div>
             </div>
-            <div class="newQuestion">
-                <h1>Formular nova pregunta</h1>
-            </div></div>
-        </div></section>
+        </section>
+
+        <section class="section main-section">
+            <div class="card has-table">
+                <header class="card-header card-header-question">
+                    <span class="icon">
+                        <i class="mdi mdi-account-multiple"></i>
+                    </span>
+                    <h1 class="card-header-title card-header-title-question">Preguntes formulades</h1>
+                </header>
+                <div>
+                    ddddd
+                </div>
+            </div>
+        </section>
     </x-slot>
 </x-app-layout>
