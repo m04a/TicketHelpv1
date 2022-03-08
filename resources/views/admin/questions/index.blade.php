82eec82bcc455610094405bcf4e6799269332ea4
nemesxv <lupenkov14@gmail.com>
2022-03-08 19:23:17 +0100

    questions view user side redone

--- resources/views/admin/questions/index.blade.php
+++ resources/views/admin/questions/index.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-user-layout>
     <x-slot name="header">
         <h1 class="title">
             Preguntes
@@ -14,6 +14,15 @@
                         <span class="icon"><i class="mdi mdi-account-multiple"></i></span>
                         Preguntes pendents
                     </p>
+                    <button class="button-table-add">
+                        <div class="inline-flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                            </svg>
+                            <span>Fer una pregunta</span>
+                        </div>
+                    </button>
                 </header>
                 <div class="card-content">
                     <table>
@@ -42,13 +51,6 @@
                                                     clip-rule="evenodd" />
                                             </svg>
                                         </button>
-                                        <button class="button-table-delete" data-target="sample-modal" type="button">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"
-                                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                            </svg>
-                                        </button>
                                     </div>
                                 </td>
                             </tr>
@@ -60,4 +62,4 @@
         </section>
 
     </x-slot>
-</x-guest-layout>
+</x-user-layout>
