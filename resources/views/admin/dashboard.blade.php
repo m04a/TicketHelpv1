3d03ac2ffe06511594fb34da8212e9b0d2c26119
m04A <molamedb@gmail.com>
2022-03-08 17:03:55 +0100

    Fet el layout nou, amb un mini dashboard amb la visa

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,12 +1,35 @@
-<x-app-layout>
+<x-user-layout>
     <x-slot name="header">
         <h1 class="title">
             Dashboard
         </h1>
     </x-slot>
     <x-slot name="slot">
-        <p>Aixo es una prova</p>
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>
+        <div class="card-container">
+            <a href="{{route('users')}}" class="card-dashboard">
+            <div class="card-item">
+                <div class="card-spacing">
+                    <div class="card-spacing-text">
+                    Incidències</div>
+                </div>
+            </div>
+            </a>
+            <a href="{{route('users')}}" class="card-dashboard">
+            <div class="card-item">
+                <div class="card-spacing">
+                    <div class="card-spacing-text">Suggerencies</div>
+                </div>
+            </div>
+            </a>
+            <a href="{{route('users')}}" class="card-dashboard">
+            <div class="card-item">
+                <div class="card-spacing">
+                    <div class="card-spacing-text">Preguntes</div>
+                </div>
+            </div>
+            </a>
+        </div>
+        </div>
     </x-slot>
-</x-app-layout>
+</x-user-layout>
 
---
+++ resources/views/layouts/user.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;600&display=swap" rel="stylesheet">
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+    <body class="user">
+        <div class="min-h-screen bg-gray-100">
+           <x-navbar></x-navbar>
+            <!-- Page Heading -->
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <section class="is-hero-bar">
+                        <div class="flex flex-col md:flex-row items-center justify-between space-y-6 md:space-y-0 text-center">
+                            {{ $header }}
+                        </div>
+                    </section>
+                </div>
+            </header>
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+    </body>
+</html>
