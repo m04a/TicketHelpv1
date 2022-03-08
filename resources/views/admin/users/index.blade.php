000ddf753a246aeb6dd1788ec94a7a3b871d23a2
m04A <molamedb@gmail.com>
2022-03-08 17:17:46 +0100

    Reverted changes of pagination

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -131,16 +131,6 @@
                     </tr>
                     </tbody>
                 </table>
-                <div class="table-pagination">
-                    <div class="flex items-center justify-between">
-                        <div class="buttons">
-                            <button type="button" class="button active">1</button>
-                            <button type="button" class="button">2</button>
-                            <button type="button" class="button">3</button>
-                        </div>
-                        <small>Pàgina 1 of 3</small>
-                    </div>
-                </div>
             </div>
         </div>
     </section>
