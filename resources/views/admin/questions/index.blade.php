fa6e460adf9f13a466abb3c6907a6615c7ecd0c2
nemesxv <lupenkov14@gmail.com>
2022-03-08 19:10:55 +0100

    WIP

--- resources/views/admin/questions/index.blade.php
+++ resources/views/admin/questions/index.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-guest-layout>
     <x-slot name="header">
         <h1 class="title">
             Preguntes
@@ -19,32 +19,20 @@
                     <table>
                         <thead>
                             <tr>
-                                <th>Usuari</th>
                                 <th>Asumpte</th>
                                 <th>Estat</th>
                                 <th>Departament</th>
-                                <th>Descripció</th>
                                 <th></th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td data-label="Usuari">Alex</td>
-                                <td data-label="Asumpte">El projector no projecta el projecte</td>
+                                <td data-label="Asumpte">Lorem ipsum</td>
                                 <td data-label="Estat">Pendent</td>
                                 <td data-label="Departament">Projectes</td>
-                                <td data-label="Descripció">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                                    do eiusmod tempo...</td>
                                 <td class="actions-cell">
                                     <div class="buttons right nowrap">
-                                        <button class="button-table-view" data-target="sample-modal-2" type="button">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"
-                                                viewBox="0 0 20 20" fill="currentColor">
-                                                <path fill-rule="evenodd"
-                                                    d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
-                                        </button>
+
                                         <button class="button-table-edit" data-target="sample-modal-2" type="button">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"
                                                 viewBox="0 0 20 20" fill="currentColor">
@@ -69,67 +57,7 @@
                 </div>
             </div>
             <br>
-            <div class="card has-table">
-                <header class="card-header">
-                    <p class="card-header-title">
-                        <span class="icon"><i class="mdi mdi-account-multiple"></i></span>
-                        Preguntes Resoltes
-                    </p>
-                </header>
-                <div class="card-content">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Usuari</th>
-                                <th>Asumpte</th>
-                                <th>Estat</th>
-                                <th>Departament</th>
-                                <th>Descripció</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td data-label="Usuari">Iker</td>
-                                <td data-label="Asumpte">bec molt? </td>
-                                <td data-label="Estat">Resolt</td>
-                                <td data-label="Departament">Projectes</td>
-                                <td data-label="Descripció">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                                    do eiusmod tempo...</td>
-                                <td class="actions-cell">
-                                    <div class="buttons right nowrap">
-                                        <button class="button-table-view" data-target="sample-modal-2" type="button">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"
-                                                viewBox="0 0 20 20" fill="currentColor">
-                                                <path fill-rule="evenodd"
-                                                    d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
-                                        </button>
-                                        <button class="button-table-edit" data-target="sample-modal-2" type="button">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"
-                                                viewBox="0 0 20 20" fill="currentColor">
-                                                <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                                                <path fill-rule="evenodd"
-                                                    d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
-                                        </button>
-                                        <button class="button-table-delete" data-target="sample-modal" type="button">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"
-                                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                            </svg>
-                                        </button>
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
         </section>
 
     </x-slot>
-</x-app-layout>
+</x-guest-layout>
