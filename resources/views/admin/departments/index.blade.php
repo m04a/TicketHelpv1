c99cadb0e187083009c0588d9aec0b19cdc61dbd
Àlex Aguilera Ullés <aaguilera@iconsl.com>
2022-03-08 17:28:33 +0100

    fix add button tables and text description table

--- resources/views/admin/departments/index.blade.php
+++ resources/views/admin/departments/index.blade.php
@@ -12,11 +12,11 @@
             <header class="card-header">
                 <div class="card-header-title"></div>
                 <button class="button-table-add">
-                    <div class="inline-flex">
+                    <div class="button-table-add-separation">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                         </svg>
-                        <span class="button-table-add-separation">Afegir Department</span>                    
+                        <span>Afegir Department</span>                    
                     </div>
                 </button>
             </header>
