82903b6cfccbd670cd32fe94ad47c35a8978ed28
nemesxv <lupenkov14@gmail.com>
2022-03-09 15:48:20 +0100

    corrected css and table header

--- resources/views/admin/devices/index.blade.php
+++ resources/views/admin/devices/index.blade.php
@@ -11,13 +11,11 @@
             <div class="card has-table">
                 <header class="card-header">
                     <p class="card-header-title">
-                        <span class="icon"><i class="mdi mdi-account-multiple"></i></span>
-                        Dispositiu
+                        Dispositius
                     </p>
                     <button class="button-table-add">
-                        <div class="inline-flex">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <div class="button-table-add-separation">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                             </svg>
                             <span>Afegir Dispositiu</span>
