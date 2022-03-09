fed5b48d9574ac787a004f6a8377f8a1bb55b274
OriolBech <you@example.com>
2022-03-09 16:19:23 +0100

    Refactor of structure folders

---
+++ resources/views/user/dashboard.blade.php
@@ -0,0 +1,35 @@
+<x-user-layout>
+    <x-slot name="header">
+        <h1 class="title">
+            Dashboard
+        </h1>
+    </x-slot>
+    <x-slot name="slot">
+        <div class="card-container">
+            <a href="{{route('users')}}" class="card-dashboard">
+            <div class="card-item">
+                <div class="card-spacing">
+                    <div class="card-spacing-text">
+                    Incidències</div>
+                </div>
+            </div>
+            </a>
+            <a href="{{route('users')}}" class="card-dashboard">
+            <div class="card-item">
+                <div class="card-spacing">
+                    <div class="card-spacing-text">Suggerencies</div>
+                </div>
+            </div>
+            </a>
+            <a href="{{route('users')}}" class="card-dashboard">
+            <div class="card-item">
+                <div class="card-spacing">
+                    <div class="card-spacing-text">Preguntes</div>
+                </div>
+            </div>
+            </a>
+        </div>
+        </div>
+    </x-slot>
+</x-user-layout>
+
