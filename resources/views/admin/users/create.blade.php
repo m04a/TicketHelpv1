3dfed4addb2f190fe1aeadf5305a891bc416ea76
Àlex Aguilera Ullés <aaguilera@iconsl.com>
2022-03-09 17:00:34 +0100

    do all components for forms and add exemple on /admin/users/create

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -8,34 +8,33 @@
 <x-create-card>
         <form method="POST" action="">
             @csrf
-            <div class="flex row">
+            <div class="content-column">
             <!-- Name User -->
-            <div>
+            <div class="column-left">
                 <x-label for="usuari" :value="__('Nom Usuari')" />
 
-                <x-input id="nom" class="block mt-4 w-full" type="text" name="nom" required autofocus />
+                <x-input id="nom" class="inpunt-content" type="text" name="nom" required autofocus />
             </div>
 
             <!-- Email User -->
-            <div>
+            <div class="column-right">
                 <x-label for="email" :value="__('Correu Electronic')" />
 
-                <x-input id="email" class="block mt-4 w-full" type="email" name="email" required autofocus />
+                <x-input id="email" class="inpunt-content" type="email" name="email" required autofocus />
             </div>
             </div>
-            <div class="flex row">
+            <div class="content-column">
                 <!-- Password -->
-                <div class="mt-4">
+                <div class="mt-4 column-left">
                     <x-label for="password" :value="__('Contrasenya')" />
 
-                    <x-input id="password" class="block mt-4 w-full"
+                    <x-input id="password" class="inpunt-content"
                                     type="password"
                                     name="password"
                                     required autocomplete="current-password" />
                 </div>
-
                 <!-- Rol User -->
-                <div class="mt-4">
+                <div class="mt-4 column-right">
                     <x-label for="rol" :value="__('Tria el Rol del Usuari')" />
                     
                     <x-select class="block mt-4 w-full">
@@ -46,7 +45,7 @@
 
                 </div>
             </div>
-            <div class="flex items-center justify-end mt-4">
+            <div class="button-create">
                 <x-button class="ml-3">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
--- resources/views/components/create-card.blade.php
+++ resources/views/components/create-card.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col pt-6 sm:pt-0 m-6">
+<div class="flex flex-col pt-6 sm:pt-0 m-6 sm:justify-center items-center">
     <div class="w-full bg-white shadow-md overflow-hidden px-6 py-4 sm:max-w-full">
         {{ $slot }}
     </div>
