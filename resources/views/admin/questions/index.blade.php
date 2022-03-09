d70e6c46a6c3e59e09787f96fb52bd6f7dd787b2
nemesxv <lupenkov14@gmail.com>
2022-03-09 15:23:25 +0100

    corrected the css, table header and added a second table in the user question view

--- resources/views/admin/questions/index.blade.php
+++ resources/views/admin/questions/index.blade.php
@@ -11,11 +11,10 @@
             <div class="card has-table">
                 <header class="card-header">
                     <p class="card-header-title">
-                        <span class="icon"><i class="mdi mdi-account-multiple"></i></span>
                         Preguntes pendents
                     </p>
                     <button class="button-table-add">
-                        <div class="inline-flex">
+                        <div class="button-table-add-separation">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
                                 viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
@@ -28,7 +27,7 @@
                     <table>
                         <thead>
                             <tr>
-                                <th>Asumpte</th>
+                                <th>Assumpte</th>
                                 <th>Estat</th>
                                 <th>Departament</th>
                                 <th></th>
@@ -36,7 +35,7 @@
                         </thead>
                         <tbody>
                             <tr>
-                                <td data-label="Asumpte">Lorem ipsum</td>
+                                <td data-label="Assumpte">Lorem ipsum</td>
                                 <td data-label="Estat">Pendent</td>
                                 <td data-label="Departament">Projectes</td>
                                 <td class="actions-cell">
@@ -59,6 +58,48 @@
                 </div>
             </div>
             <br>
+            <div class="card has-table">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        Preguntes resoltes
+                    </p>
+                </header>
+                <div class="card-content">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Assumpte</th>
+                                <th>Estat</th>
+                                <th>Departament</th>
+                                <th>Gestor</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label="Assumpte">Lorem ipsum</td>
+                                <td data-label="Estat">Resolt</td>
+                                <td data-label="Departament">Projectes</td>
+                                <td data-label="Gestor">Oriol</td>
+                                <td class="actions-cell">
+                                    <div class="buttons right nowrap">
+
+                                        <button class="button-table-edit" data-target="sample-modal-2" type="button">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"
+                                                viewBox="0 0 20 20" fill="currentColor">
+                                                <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                                                <path fill-rule="evenodd"
+                                                    d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </section>
 
     </x-slot>
