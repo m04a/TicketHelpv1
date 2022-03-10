abdaccd043fe1f1f00d9e4f4fad1d04b7a939858
OriolBech <you@example.com>
2022-03-10 15:19:31 +0100

    Added role middleware

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,11 +1,12 @@
 <x-app-layout>
+    
     <x-slot name="header">
         <h1 class="title">
             Dashboard
         </h1>
     </x-slot>
-    <x-slot name="slot">
 
+    <x-slot name="slot">
 
         <section class="section main-section">
         
