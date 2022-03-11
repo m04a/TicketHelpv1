2e118c2c31845df72792cee9ead07827b8c79f45
George11849 <george11849@gmail.com>
2022-03-11 18:21:57 +0100

    Feature Question Add Admin

--- resources/views/admin/questions/create.blade.php
+++ resources/views/admin/questions/create.blade.php
@@ -1,61 +1,71 @@
 <x-app-layout>
-
-        <x-slot name="header">
-            <h1 class="title">
-                Afegir Pregunta
-            </h1>
-        </x-slot>
-<x-create-card>
-        <form method="POST" action="">
+    <x-slot name="header">
+        <h1 class="title">
+            Crear una Pregunta
+        </h1>
+    </x-slot>
+    <x-slot name="slot">
+        <x-create-card>
+            <form method="POST" action="">
             @csrf
+
+            <!-- Id  -->
             <div class="content-column">
-            <!-- Name User -->
-            <div class="column-left">
-                <x-label for="usuari" :value="__('Nom Usuari')" />
+                <div class="column-left">
+                    <x-label class="label" for="assumpte" :value="__('Codi Pregunta')" />
 
-                <x-input id="nom" class="inpunt-content" type="text" name="nom" required autofocus />
-            </div>
+                    <x-input id="nom" class="block mt-4 w-full" type="text" name="id" required autofocus />
+                </div>
 
-            <!-- Status Breakdown -->
-            <div class="column-right">
-                    <x-label for="rol" :value="__('Estat Incidència')" />
-                    
-                    <x-select class="block mt-4 w-full">
-                        <option value="1">Sense Assignar</option>
-                        <option value="2">En procés</option>
-                    </x-select>
+            <!-- Tittle  -->
+
+                <div class="column-right">
+                    <x-label class="label" for="assumpte" :value="__('Títol')" />
+
+                    <x-input id="nom" class="block mt-4 w-full" type="text" name="titol" required autofocus />
                 </div>
             </div>
+            <!-- Department -->
             <div class="content-column">
-                <!-- Subject -->
-                <div class="mt-4 column-left">
-                    <x-label for="assumpte" :value="__('Assumpte')" />
+                <div class="column-left mt-4">
+                    <x-label for="Tipus" :value="__('Departament')" />
 
-                    <x-input id="assumpte" class="inpunt-content" type="text" name="assumpte" required autofocus />
-
-                </div>
-                <!-- Rol User -->
-                <div class="mt-4 column-right">
-                    <x-label for="rol" :value="__('Departament')" />
-                    
                     <x-select class="block mt-4 w-full">
-                        <option value="1">Departament Informàtica</option>
+                        <option value="1">Informatica</option>
                         <option value="2">Consergeria</option>
                         <option value="3">Secretaria</option>
-                        <option value="4">Direcció</option>
-                        <option value="5">Altres</option>
                     </x-select>
+                </div>
 
+            <!-- Id User -->
+                 <div class="column-right mt-4">
+                    <x-label class="label" for="assumpte" :value="__('Codi Usuari')" />
+
+                    <x-input id="nom" class="block mt-4 w-full" type="text" name="codiU" required autofocus />
                 </div>
             </div>
-            <div class="button-create">
-                <x-button class="ml-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
-                    </svg>
-                    {{ __('Crear Incidència') }}
-                </x-button>
-            </div>
-        </form>
-    </x-create-card>
+            <!-- Text of the question -->
+                <div class="mt-4">
+                    <x-label class="label" for="missatge" :value="__('Pregunta')" />
+
+                    <textarea class="textarea" type="text" name="pregunta" required autofocus></textarea>
+                </div>
+
+        
+                <div class="flex items-center justify-end mt-4">
+                    <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+                        <input type="checkbox" name="toggle" id="toggle" class="toggle-checkbox focus:border-red-600 focus:ring-red-600 focus:ring-0 absolute block w-6 h-6 rounded-full bg-white appearance-none cursor-pointer"/>
+                        <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 focus:ring-none cursor-pointer"></label>
+                    </div>
+                    <label for="toggle" class="text-xs text-gray-800">Estat</label>
+                    <x-button class="ml-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
+                        </svg>
+                        {{ __('Crear Pregunta') }}
+                    </x-button>
+                </div>
+            </form>
+        </x-create-card>
+    </x-slot>
 </x-app-layout>
