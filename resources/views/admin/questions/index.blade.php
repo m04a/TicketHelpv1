a42422bc262640ebd37d553eb98afcfc8a017691
iramajo <ikersa.220802@gmail.com>
2022-03-16 16:16:58 +0100

    Create Method Index Questions Admin and User

--- resources/views/admin/questions/index.blade.php
+++ resources/views/admin/questions/index.blade.php
@@ -33,11 +33,12 @@
                         </tr>
                     </thead>
                     <tbody>
+                        @foreach ($questions as $question)
                     <tr>
-                        <td data-label="Assumpte">Problemes amb el projector</td>
-                        <td data-label="Estat">Pendent</td>
-                        <td data-label="Departament">Projectes</td>
-                        <td data-label="Usuari">Alex</td>
+                        <td data-label="Assumpte">{{ $question['title']}}</td>
+                        <td data-label="Estat">{{ $question['status']}}</td>
+                        <td data-label="Departament">{{ $question['department_id']}}</td>
+                        <td data-label="Usuari">{{ $question['user_id']}}</td>
                         <td class="actions-cell">
                             <div class="buttons right nowrap">
 
@@ -56,92 +57,7 @@
                             </div>
                         </td>
                     </tr>
-                    <tr>
-                        <td data-label="Assumpte">Problemes amb el projector</td>
-                        <td data-label="Estat">Pendent</td>
-                        <td data-label="Departament">Projectes</td>
-                        <td data-label="Usuari">Alex</td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                            <button class="button-table-view"  data-target="sample-modal-2" type="button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                                        <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-                                <button class="button-table-delete" data-target="sample-modal" type="button">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                </svg>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td data-label="Assumpte">Problemes amb el projector</td>
-                        <td data-label="Estat">Pendent</td>
-                        <td data-label="Departament">Projectes</td>
-                        <td data-label="Usuari">Alex</td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                                            <button class="button-table-view"  data-target="sample-modal-2" type="button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                                        <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-                                        </button>
-                                <button class="button-table-delete" data-target="sample-modal" type="button">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                </svg>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td data-label="Assumpte">Problemes amb el projector</td>
-                        <td data-label="Estat">Pendent</td>
-                        <td data-label="Departament">Projectes</td>
-                        <td data-label="Usuari">Alex</td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                            <button class="button-table-view"  data-target="sample-modal-2" type="button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                                        <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-                                        </button>
-                                <button class="button-table-delete" data-target="sample-modal" type="button">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                </svg>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td data-label="Assumpte">Problemes amb el projector</td>
-                        <td data-label="Estat">Pendent</td>
-                        <td data-label="Departament">Projectes</td>
-                        <td data-label="Usuari">Alex</td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                            <button class="button-table-view"  data-target="sample-modal-2" type="button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                                        <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-                                <button class="button-table-delete" data-target="sample-modal" type="button">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                </svg>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
+                    @endforeach
                     </tbody>
                 </table>
             </div>
@@ -167,13 +83,13 @@
                         </tr>
                     </thead>
                     <tbody>
-                    
+                        @foreach ($questions as $question)
                     <tr>
-                        <td data-label="Assumpte">Problemes amb el projector</td>
-                        <td data-label="Estat">Resolta</td>
-                        <td data-label="Departament">Projectes</td>
-                        <td data-label="Usuari">Alex</td>
-                        <td data-label="Manager">Iker</td>
+                        <td data-label="Assumpte">{{ $question['title']}}</td>
+                        <td data-label="Estat">{{ $question['status']}}</td>
+                        <td data-label="Departament">{{ $question['department_id']}}</td>
+                        <td data-label="Usuari">{{ $question['user_id']}}</td>
+                        <td data-label="Manager">{{ $question['manager_id']}}</td>
                         <td class="actions-cell">
                             <div class="buttons right nowrap">
                             <button class="button-table-view"  data-target="sample-modal-2" type="button">
@@ -190,55 +106,23 @@
                             </div>
                         </td>
                     </tr>
-                
-                    <tr>
-                        
-                        <td data-label="Assumpte">Desinfecció no realitzada a l'aula 306</td>
-                        <td data-label="Estat">Resolta</td>
-                        <td data-label="Departament">Projectes</td>
-                        <td data-label="Usuari">Manel Martín</td>
-                        <td data-label="Manager">Kirill Lupenkov</td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                            <button class="button-table-view"  data-target="sample-modal-2" type="button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                                        <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-                                <button class="button-table-delete" data-target="sample-modal" type="button">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                </svg>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-
-                        <td data-label="Assumpte">Porta trencada 2on Eso</td>
-                        <td data-label="Estat">Resolta</td>
-                        <td data-label="Departament">Projectes</td>
-                        <td data-label="Usuari">Maria B.</td>
-                        <td data-label="Manager">Maria B.</td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                            <button class="button-table-view"  data-target="sample-modal-2" type="button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                                        <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-                                <button class="button-table-delete" data-target="sample-modal" type="button">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                </svg>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
+                    @endforeach
                     </tbody>
                 </table>
+                <x-pagination>
+                        @for($i = 0; $i < $questions->lastPage(); $i++)
+                            <div class="buttons">
+                                <a class="pagination-next m-2" href="{{ url('/admin/questions?page=' . $i+1) }}" >
+                                    @if($questions->currentPage() == $i+1) 
+                                    <button type="button" class="button active">{{ $i+1 }}</button>
+                                    @else
+                                    <button type="button" class="button">{{ $i+1 }}</button>
+                                    @endif
+                                </a>
+                            </div>
+                        @endfor
+                        <small class="flex w-full justify-end mr-0.5">Pàgina {{ $questions->currentPage() }} de {{ $questions->lastPage() }} </small>
+                    </x-pagination>
             </div>
         </div>
     </section>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BreakdownController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SuggestionController;
+use App\Http\Controllers\QuestionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -95,9 +96,7 @@ Route::middleware(['auth'])->group(function () {
 
         ///////////////////////////////////////////////////
 
-        Route::get('/admin/questions', function () {
-            return view('admin/questions/index');
-        })->name('admin.questions');
+        Route::get('/admin/questions' , [QuestionController::class, "index"])->name('admin.questions');
 
         Route::get('/admin/questions/create', function () {
             return view('admin/questions/create');
@@ -176,9 +175,8 @@ Route::middleware(['auth'])->group(function () {
         return view('user/questions/create');
     })->name('user.questions.create');
 
-    Route::get('/user/questions/list', function () {
-        return view('user/questions/list');
-    })->name('user.questions');
+    Route::get('/user/questions/list', [QuestionController::class, 'index'])->name('user.questions.list');
+
 
     ///////////////////////////////////////////////////
 
