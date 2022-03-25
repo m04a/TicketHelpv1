71491b59f1bf84fc42e965644cae21f355164250
m04A <molamedb@gmail.com>
2022-03-25 19:16:26 +0100

    Added test user

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OauthController extends Controller
 {
     public static function store($user){
+        dd(Auth::user()->id);
         dd($user->email);
+
     }
 }
