1183a5edf7c0a39506564c3fb090126331f58c2b
m04A <molamedb@gmail.com>
2022-03-27 14:03:50 +0200

    Solved principal page bug and added views to return

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -33,7 +33,7 @@ class OauthController extends Controller
            $oauthObjectModel->mail = $user->email;
 
            if($oauthObjectModel->save()){
-               return back()->with('success','S\'han actualitzat les dades de l\'usuari.');
+               return redirect('/admin/profile/')->with('success','S\'han actualitzat les dades de l\'usuari.');
            }
           }else{
            $vinculation = Service_oauth::create([
@@ -43,11 +43,11 @@ class OauthController extends Controller
            ]);
 
            if($vinculation){
-               return back()->with('success','S\'han creat les dades de vinculació l\'usuari.');
+               return redirect('/admin/profile/')->with('success','S\'han creat les dades de vinculació l\'usuari.');
            }
        }
         }else{
-            return back()->with('success','No s\'han creat les dades de vinculació l\'usuari.');
+            return redirect('/admin/profile/')->with('success','No s\'han creat les dades de vinculació l\'usuari.');
         }
     }
     public function redirectProvider($provider){
