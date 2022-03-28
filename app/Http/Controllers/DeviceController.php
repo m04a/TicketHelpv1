79930690190b0aeaf248b34db31161f520690ef3
m04A <molamedb@gmail.com>
2022-03-28 17:26:26 +0200

    Added the test to check the endpoint

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -132,7 +132,7 @@ class DeviceController extends Controller
     }
     public function graph4(){
 
-        $devices = Device::all();
+        $devices = Device::all()->toArray();
         dd($devices);
         foreach ($types as $item){
             $data[$item->zone->id];
