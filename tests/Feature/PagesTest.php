4c6ea7fe8d3c4538a5fd7676191e2ac40c36c50a
m04A <molamedb@gmail.com>
2022-03-31 17:11:51 +0200

    Unitary tests made

---
+++ tests/Feature/PagesTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PagesTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_root()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_guides()
+    {
+        $response = $this->get('/guides');
+
+        $response->assertStatus(200);
+    }
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_profile()
+    {
+        $this->withoutMiddleware();
+
+        $user = User::firstWhere('role_id' ,3);
+
+        $response = $this->actingAs($user)->get('/admin/profile');
+
+        $response->assertSee('Editar Usuari');
+    }
+
+    public function test_dashboard()
+    {
+        $this->withoutMiddleware();
+
+        $user = User::firstWhere('role_id' ,3);
+
+        $response = $this->actingAs($user)->get('/admin/dashboard');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_doubts()
+    {
+        $this->withoutMiddleware();
+
+        $user = User::firstWhere('role_id' ,3);
+
+        $response = $this->actingAs($user)->get('/admin/questions');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test departaments
+     *
+     * @return void
+     */
+    public function test_departament()
+    {
+        $this->withoutMiddleware();
+
+        $user = User::firstWhere('role_id' ,3);
+
+        $response = $this->actingAs($user)->get('/admin/departments');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Send data to test the creation of a question
+     *
+     * @return void
+     */
+    public function test_send_breakdown()
+    {
+        $this->withoutMiddleware();
+
+        $user = User::firstWhere('role_id' ,1);
+
+        $response = $this->actingAs($user)->post('/user/questions/create', [
+            "title" => "Problemes amb el projector",
+            "description" => "Prova test unitari",
+            "status" => 1,
+            "department_id" => 1,
+            "user_id" => 1,
+            "manager_id" => 1
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+
+
+}
