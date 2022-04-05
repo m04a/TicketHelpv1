03ac1645e532f15f371ad234dd33c2b55abc3521
iramajo <ikersa.220802@gmail.com>
2022-04-05 18:38:40 +0200

    Change Dashboard Styles

--- app/Http/Controllers/HomePage.php
+++ app/Http/Controllers/HomePage.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Question;
+use App\Models\Breakdown;
 
 class HomePage extends Controller
 {
@@ -17,12 +19,18 @@ class HomePage extends Controller
 
 
         if (Auth::check() && Auth::user()->role_id > 1){
-            return view('admin.dashboard');
+            $data['questionCount'] = Question::where('status',1)->count();
+            $data['questionCountPendents'] = Question::where('status',2)->count();
+            $data['questionCountResoltes'] = Question::where('status',3)->count();
+            $data['breakdownCount'] = Breakdown::where('status',1)->count();
+            $data['breakdownCountPendents'] = Breakdown::where('status',2)->count();
+            $data['breakdownCountResoltes'] = Breakdown::where('status',3)->count();
+            return view('admin.dashboard', $data);
         }elseif(Auth::check() && Auth::user()->role_id == 1){
             return view('user.dashboard');
         }else{
             return view('auth/login');
         }
-        
+
     }
 }
