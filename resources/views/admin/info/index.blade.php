ffc1dc18a05088182008f3efb3fe2b7d6fd5fdea
OriolBech <you@example.com>
2022-04-08 16:29:33 +0200

    Added info page

--- resources/views/admin/info/index.blade.php
+++ resources/views/admin/info/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h1 class="title">
-            Nosaltres
+            Equip
         </h1>
     </x-slot>
     <x-slot name="slot">
@@ -11,44 +11,45 @@
                 
                 <div class="card-info bg-white shadow-md">
                     <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar George">
-                    <h3 class="names">JORGE LUIS MARTINEZ</h3>
+                    <h3 class="names">Jorge Luis Martinez</h3>
                     <a href="https://github.com/George11849" class="gith">GitHub</a>
                 </div>
                 
                 <div class="card-info bg-white shadow-md">
-                    <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Iker">
+                    <img src="https://i.postimg.cc/T19BfLjM/Avatar-Maker.png" alt="Avatar Iker">
                     <h3 class="names">Iker Ramajo</h3>
                     <a href="https://github.com/iramajo" class="gith">GitHub</a>
                 </div>
-                
+
                 <div class="card-info bg-white shadow-md">
-                    <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar JuanK">
-                    <h3 class="names">Juan Carlos Burria</h3>
-                    <a href="https://github.com/Juanka007" class="gith">GitHub</a>
+                    <img src="https://i.postimg.cc/KY27rqSf/Avatar-Maker-3.png" alt="Avatar Mohamed">
+                    <h3 class="names">Mohamed Bourarach</h3>
+                    <a href="https://github.com/m04a" class="gith">GitHub</a>
                 </div>
                 
+                
                 <div class="card-info bg-white shadow-md">
-                    <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Kirill">
+                    <img src="https://i.postimg.cc/Rh5xc6r8/Avatar-Maker-2.png" alt="Avatar Kirill">
                     <h3 class="names">Kirill Lupenkov</h3>
                     <a href="https://github.com/nemesxv" class="gith">GitHub</a>
                 </div>
                 
                 <div class="card-info bg-white shadow-md">
-                    <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Oriol">
+                    <img src="https://i.postimg.cc/TYNH0XC9/Avatar-Maker-3.png" alt="Avatar Oriol">
                     <h3 class="names">Oriol Bech</h3>
                     <a href="https://github.com/OriolBech" class="gith">GitHub</a>
                 </div>
                 
                 <div class="card-info bg-white shadow-md">
-                    <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Alex">
-                    <h3 class="names">Alex Aliguera</h3>
+                    <img src="https://i.postimg.cc/fbfcHcMN/Avatar-Maker-2.png" alt="Avatar Alex">
+                    <h3 class="names">Alex Aguilera</h3>
                     <a href="https://github.com/Luky7600" class="gith">GitHub</a>
                 </div>
 
                 <div class="card-info bg-white shadow-md">
-                    <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Mohamed">
-                    <h3 class="names">Mohamed Bourarach</h3>
-                    <a href="https://github.com/m04a" class="gith">GitHub</a>
+                    <img src="https://i.postimg.cc/wBDHykDD/Avatar-Maker-1.png" alt="Avatar JuanK">
+                    <h3 class="names">Juan Carlos Burria</h3>
+                    <a href="https://github.com/Juanka007" class="gith">GitHub</a>
                 </div>
                 
             </div>    
