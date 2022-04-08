58962db3663230c8cc79251317aa3a81f1e57824
OriolBech <you@example.com>
2022-04-08 16:03:19 +0200

    Fixed some bugs #415

--- resources/views/admin/info/index.blade.php
+++ resources/views/admin/info/index.blade.php
@@ -7,45 +7,45 @@
     <x-slot name="slot">
     
         <section class="section main-section">
-            <div class="flex flex-wrap">
+            <div class="flex flex-wrap gap-4">
                 
-                <div class="card-info">
+                <div class="card-info bg-white shadow-md">
                     <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar George">
                     <h3 class="names">JORGE LUIS MARTINEZ</h3>
                     <a href="https://github.com/George11849" class="gith">GitHub</a>
                 </div>
                 
-                <div class="card-info">
+                <div class="card-info bg-white shadow-md">
                     <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Iker">
                     <h3 class="names">Iker Ramajo</h3>
                     <a href="https://github.com/iramajo" class="gith">GitHub</a>
                 </div>
                 
-                <div class="card-info">
+                <div class="card-info bg-white shadow-md">
                     <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar JuanK">
                     <h3 class="names">Juan Carlos Burria</h3>
                     <a href="https://github.com/Juanka007" class="gith">GitHub</a>
                 </div>
                 
-                <div class="card-info">
+                <div class="card-info bg-white shadow-md">
                     <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Kirill">
                     <h3 class="names">Kirill Lupenkov</h3>
                     <a href="https://github.com/nemesxv" class="gith">GitHub</a>
                 </div>
                 
-                <div class="card-info">
+                <div class="card-info bg-white shadow-md">
                     <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Oriol">
                     <h3 class="names">Oriol Bech</h3>
                     <a href="https://github.com/OriolBech" class="gith">GitHub</a>
                 </div>
                 
-                <div class="card-info">
+                <div class="card-info bg-white shadow-md">
                     <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Alex">
                     <h3 class="names">Alex Aliguera</h3>
                     <a href="https://github.com/Luky7600" class="gith">GitHub</a>
                 </div>
 
-                <div class="card-info">
+                <div class="card-info bg-white shadow-md">
                     <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Mohamed">
                     <h3 class="names">Mohamed Bourarach</h3>
                     <a href="https://github.com/m04a" class="gith">GitHub</a>
