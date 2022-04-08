0fd58c0264ff497ac62d9ff215ccfaee0966ed7e
OriolBech <you@example.com>
2022-04-08 17:40:51 +0200

    Added info team

--- resources/views/admin/info/index.blade.php
+++ resources/views/admin/info/index.blade.php
@@ -1,54 +1,55 @@
 <x-app-layout>
     <x-slot name="header">
         <h1 class="title">
-            Nosaltres
+            Equip
         </h1>
     </x-slot>
     <x-slot name="slot">
     
         <section class="section main-section">
-            <div class="flex flex-wrap">
+            <div class="flex flex-wrap gap-4">
                 
-                <div class="card-info">
+                <div class="card-info bg-white shadow-md">
                     <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar George">
-                    <h3 class="names">JORGE LUIS MARTINEZ</h3>
-                    <a href="https://github.com/George11849" class="gith" target="_blank">GitHub</a>
+                    <h3 class="names">Jorge Luis Martinez</h3>
+                    <a href="https://github.com/George11849" class="gith">GitHub</a>
                 </div>
                 
-                <div class="card-info">
-                    <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Iker">
+                <div class="card-info bg-white shadow-md">
+                    <img src="https://i.postimg.cc/T19BfLjM/Avatar-Maker.png" alt="Avatar Iker">
                     <h3 class="names">Iker Ramajo</h3>
                     <a href="https://github.com/iramajo" class="gith" target="_blank">GitHub</a>
                 </div>
-                
-                <div class="card-info">
-                    <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar JuanK">
-                    <h3 class="names">Juan Carlos Burria</h3>
-                    <a href="https://github.com/Juanka007" class="gith" target="_blank">GitHub</a>
+
+                <div class="card-info bg-white shadow-md">
+                    <img src="https://i.postimg.cc/KY27rqSf/Avatar-Maker-3.png" alt="Avatar Mohamed">
+                    <h3 class="names">Mohamed Bourarach</h3>
+                    <a href="https://github.com/m04a" class="gith">GitHub</a>
                 </div>
                 
-                <div class="card-info">
-                    <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Kirill">
+                
+                <div class="card-info bg-white shadow-md">
+                    <img src="https://i.postimg.cc/Rh5xc6r8/Avatar-Maker-2.png" alt="Avatar Kirill">
                     <h3 class="names">Kirill Lupenkov</h3>
                     <a href="https://github.com/nemesxv" class="gith" target="_blank">GitHub</a>
                 </div>
                 
-                <div class="card-info">
-                    <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Oriol">
+                <div class="card-info bg-white shadow-md">
+                    <img src="https://i.postimg.cc/TYNH0XC9/Avatar-Maker-3.png" alt="Avatar Oriol">
                     <h3 class="names">Oriol Bech</h3>
                     <a href="https://github.com/OriolBech" class="gith" target="_blank">GitHub</a>
                 </div>
                 
-                <div class="card-info">
-                    <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Alex">
+                <div class="card-info bg-white shadow-md">
+                    <img src="https://i.postimg.cc/fbfcHcMN/Avatar-Maker-2.png" alt="Avatar Alex">
                     <h3 class="names">Alex Aguilera</h3>
-                    <a href="https://github.com/Luky7600" class="gith" target="_blank">GitHub</a>
+                    <a href="https://github.com/Luky7600" class="gith">GitHub</a>
                 </div>
 
-                <div class="card-info">
-                    <img src="https://i.postimg.cc/dtGBmVVR/Avatar-George.png" alt="Avatar Mohamed">
-                    <h3 class="names">Mohamed Bourarach</h3>
-                    <a href="https://github.com/m04a" class="gith" target="_blank">GitHub</a>
+                <div class="card-info bg-white shadow-md">
+                    <img src="https://i.postimg.cc/wBDHykDD/Avatar-Maker-1.png" alt="Avatar JuanK">
+                    <h3 class="names">Juan Carlos Burria</h3>
+                    <a href="https://github.com/Juanka007" class="gith">GitHub</a>
                 </div>
                 
             </div>    
